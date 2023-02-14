e886093cc67399a4d176cd0c34b8c13ce959a6dd
KimYeongIm0 <ygim4029@gmail.com>
2023-02-14 13:56:15 +0900

    eslintrc,prettierrc,redux,styled-components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,84 +1,80 @@
-import React ,{useEffect}from 'react';
-import {v4 as uuidv4} from 'uuid'
+import React, { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 
-const Form = ({title,setTitle,todos,setTodos,editTodo,setEditTodo}) => {
-    
+// eslint-disable-next-line react/prop-types
+const Form = ({ title, setTitle, setTodos, editTodo }) => {
+  const onInputChange = (event) => {
+    setTitle(event.target.value);
+  };
+  // const onFormSubmit=(event)=>{
+  //     if(pop === true){
+  //         setPop(!pop)
+  //         event.preventDefault();
+  //         setTodos([...todos,{id:uuidv4(),title:input, complete:false}]);
+  //         setInput("")
+  //     }else{
+  //         event.preventDefault();
+  //         setTodos([...todos,{id:uuidv4(),title:input, complete:false}]);
+  //         setInput("")
+  //     }
+  // }
 
-    const onInputChange=(event)=>{
-        setTitle(event.target.value)
-    };
+  const readList = async () => {
+    const { data } = await axios.get("http://localhost:5000/todos");
+    setTodos(data);
+  };
+  useEffect(() => {
+    (async () => {
+      await readList();
+    })();
+  }, []);
 
-    // const onFormSubmit=(event)=>{
-    //     if(pop === true){
-    //         setPop(!pop)
-    //         event.preventDefault();
-    //         setTodos([...todos,{id:uuidv4(),title:input, complete:false}]);
-    //         setInput("")
-    //     }else{
-    //         event.preventDefault();
-    //         setTodos([...todos,{id:uuidv4(),title:input, complete:false}]);
-    //         setInput("")
-    //     }
-    
-    // }
-
-    const readList = async()=>{
-        const {data} = await axios.get("http://localhost:5000/todos")
-        setTodos(data)
-    };
-    useEffect(()=>{
-        (async ()=>{
-            await readList();
-        })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-
-    useEffect(()=>{
-        if(editTodo){
-            setTitle(editTodo.title)
-        }else{
-            setTitle("")
-        }
-    },[setTitle,editTodo])
-
-        const onFormSubmit = async()=>{
-            if(!editTodo ){
-                await axios.post("http://localhost:5000/todos",{
-                    id: uuidv4(),
-                    title,
-                    complete:false
-                });
-                setTitle("")
-            }else{
-                updateTodo(editTodo.id)
-                }    
-            }
-    const updateTodo=async(id)=>{
-        await axios.patch(`http://localhost:5000/todos/${id}`,{
-            title:title
-        })
-        await readList()
+  useEffect(() => {
+    if (editTodo) {
+      // eslint-disable-next-line react/prop-types
+      setTitle(editTodo.title);
+    } else {
+      setTitle("");
     }
-    
-        
-    
-    return (
-        <form onSubmit={onFormSubmit}>
-            <input 
-                type='text'
-                placeholder='Enter a Todo..'
-                className='task-input'
-                value={title}
-                required
-                onChange={onInputChange}
-                />
-            <button className='button-add' type='submit'>
-                Add
-            </button>
-        </form>
-        
-    );
+  }, [setTitle, editTodo]);
+
+  const onFormSubmit = async () => {
+    if (!editTodo) {
+      await axios.post("http://localhost:5000/todos", {
+        id: uuidv4(),
+        title,
+        complete: false,
+      });
+      setTitle("");
+    } else {
+      // eslint-disable-next-line react/prop-types
+      updateTodo(editTodo.id);
+    }
+  };
+  const updateTodo = async (id) => {
+    await axios.patch(`http://localhost:5000/todos/${id}`, {
+      title: title,
+    });
+    await readList();
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a Todo.."
+        className="task-input"
+        value={title}
+        required
+        onChange={onInputChange}
+      />
+      <button className="button-add" type="submit">
+        {" "}
+        Add{" "}
+      </button>
+    </form>
+  );
 };
 
 export default Form;
